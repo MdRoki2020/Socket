88e46485abba1cde33b5ed2d3adc29931640602b
Roki <mroki815@gmail.com>
2023-03-22 22:38:30 +0600

    custome Event

---
+++ custom_Event/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(expressServer);
+
+io.on('connection', (socket) => {
+  console.log("User Connected")
+
+
+setInterval(()=>{
+  let d=new Date();
+  let t=d.getSeconds();
+  socket.emit("MyEvent",t);
+},1000);
+
+
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+expressServer.listen(3000, () => {
+  console.log("Server Run At 3000");
+});
