9cde24e32b88ffecf2dd1535f1c743ee19e9512f
Roki <mroki815@gmail.com>
2023-03-25 23:32:07 +0600

    custome event transfer data clint to server

---
+++ client_to_Server_Custom_Event/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(expressServer);
+
+io.on('connection', (socket) => {
+  console.log("User Connected")
+
+
+socket.on('message',(msg)=>{
+  console.log(msg);
+})
+
+
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+expressServer.listen(3000, () => {
+  console.log("Server Run At 3000");
+});
