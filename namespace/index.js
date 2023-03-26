344f9bb03e2adb705e6c495d4d88aad2f6e7fe77
Roki <mroki815@gmail.com>
2023-03-26 23:28:52 +0600

    NameSpace

--- namespace/index.js
+++ namespace/index.js
@@ -1,3 +1,4 @@
+const { Socket } = require('dgram');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -5,16 +6,20 @@ const expressServer = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(expressServer);
 
-io.on('connection', (socket) => {
-  console.log("User Connected")
 
-
-socket.on('message',(msg)=>{
-  console.log(msg);
+let buyNsp=io.of("/buy");
+buyNsp.on('connection',(Socket)=>{
+  buyNsp.emit("MyEvent","hello Buy"); 
 })
 
 
+let sellNsp=io.of("/sell");
+sellNsp.on('connection',(Socket)=>{
+  sellNsp.emit("MyEvent","hello Sell");
 })
+
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
