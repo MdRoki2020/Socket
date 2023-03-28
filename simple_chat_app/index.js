40f440fe3b1d0614a331be55abbd77d5aadfba57
Roki <mroki815@gmail.com>
2023-03-28 23:42:06 +0600

    server connected

--- simple_chat_app/index.js
+++ simple_chat_app/index.js
@@ -3,11 +3,18 @@ const app=express();
 const http=require('http');
 const expressServer=http.createServer(app);
 
+const {Server}= require('socket.io');
+let io=new Server(expressServer);
+
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname+"/index.html")
 })
 
+
+io.on('connection',(socket)=>{
+    console.log("new user connected");
+})
 
 expressServer.listen(3000,()=>{
     console.log('server run at 3000');
